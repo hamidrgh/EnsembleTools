5beee1b2ed0e044aa139b4f0e918df897fcb8dec
Nikola Ivanović <nikola.iv.99@gmail.com>
2024-03-11 12:36:41 +0100

    Expanded to multiple entries and ensembles

--- refactored/ensemble_analysis.py
+++ refactored/ensemble_analysis.py
@@ -1,38 +1,47 @@
 from api_client import APIClient
-import os
 from utils import extract_tar_gz, read_file
+from ped_entry import PedEntry
+import os
 
 class EnsembleAnalysis:
-    def __init__(self, ped_id, ensemble_id, data_dir) -> None:
-        api_client = APIClient()
-        url = f'https://deposition.proteinensemble.org/api/v1/entries/{ped_id}/ensembles/{ensemble_id}/ensemble-pdb'
-        headers = {'accept': '*/*'}
+    def __init__(self, ped_entries:PedEntry, data_dir:str):
+        self.ped_entries = ped_entries
+        self.data_dir = data_dir
+        self.api_client = APIClient()
 
-        response = api_client.perform_get_request(url, headers=headers)
-        if response:
-            content_length = response.headers.get('content-length')
-            content_type = response.headers.get('content-type')
-            print("Content-Length:", content_length)
-            print("Content-Type:", content_type)
+    def run_analysis(self):
+        for ped_entry in self.ped_entries:
+            ped_id = ped_entry.ped_id
+            ensemble_ids = ped_entry.ensemble_ids
+            for ensemble_id in ensemble_ids:
+                url = f'https://deposition.proteinensemble.org/api/v1/entries/{ped_id}/ensembles/{ensemble_id}/ensemble-pdb'
+                headers = {'accept': '*/*'}
 
-            # Specify the path to save the downloaded file
-            generated_name = f'{ped_id}_{ensemble_id}'
-            filename = f'{generated_name}.tar.gz'
-            tar_gz_file = os.path.join(data_dir, filename)
+                response = self.api_client.perform_get_request(url, headers=headers)
+                if response:
+                    content_length = response.headers.get('content-length')
+                    content_type = response.headers.get('content-type')
+                    print("Content-Length:", content_length)
+                    print("Content-Type:", content_type)
 
-            # Download and save the response content to a file
-            api_client.download_response_content(response, tar_gz_file)
-            api_client.close_session()
+                    generated_name = f'{ped_id}_{ensemble_id}'
+                    filename = f'{generated_name}.tar.gz'
+                    tar_gz_file = os.path.join(self.data_dir, filename)
 
-            output_dir = os.path.join(data_dir, generated_name)
-            pdb_filename = 'pdbfile.pdb'
+                    # Download and save the response content to a file
+                    self.api_client.download_response_content(response, tar_gz_file)
+                    self.api_client.close_session()
 
-            # Extract the .tar.gz file
-            extract_tar_gz(tar_gz_file, output_dir)
+                    output_dir = os.path.join(self.data_dir, generated_name)
+                    pdb_filename = 'pdbfile.pdb'
 
-            pdb_file = os.path.join(output_dir, pdb_filename)
-            if os.path.exists(pdb_file):
-                print("Reading contents of", pdb_filename)
-                read_file(pdb_file)
-            else:
-                print("File not found:", pdb_file)
+                    # Extract the .tar.gz file
+                    extract_tar_gz(tar_gz_file, output_dir)
+
+                    pdb_file = os.path.join(output_dir, pdb_filename)
+                    if os.path.exists(pdb_file):
+                        print("Reading contents of", pdb_filename)
+                        read_file(pdb_file)
+                    else:
+                        print("File not found:", pdb_file)
+
---
+++ refactored/ped_entry.py
@@ -0,0 +1,4 @@
+class PedEntry:
+    def __init__(self, ped_id, ensemble_ids):
+        self.ped_id = ped_id
+        self.ensemble_ids = ensemble_ids
