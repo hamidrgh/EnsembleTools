98b767b4d40ee3d2c0047b69a57d16f0c9f8c98e
Nikola Ivanović <nikola.iv.99@gmail.com>
2024-03-21 12:34:13 +0100

    Expanded data loading to ensemble directories

--- refactored/ensemble_analysis.py
+++ refactored/ensemble_analysis.py
@@ -2,7 +2,6 @@ import re
 from api_client import APIClient
 from visualization import dimenfix_cluster_scatter_plot, dimenfix_cluster_scatter_plot_2, dimenfix_scatter_plot, dimenfix_scatter_plot_2, pca_correlation_plot, pca_cumulative_explained_variance, pca_plot_1d_histograms, pca_plot_2d_landscapes, pca_rg_correlation, plot_average_dmap_comparison, plot_cmap_comparison, plot_distance_distribution_multiple, trajectories_plot_asphericity, trajectories_plot_density, trajectories_plot_dihedrals, trajectories_plot_prolateness, trajectories_plot_relative_helix_content_multiple_proteins, trajectories_plot_rg_comparison, trajectories_plot_total_sasa, trajectories_scatter_prolateness, tsne_ramachandran_plot, tsne_ramachandran_plot_density, tsne_scatter_plot, tsne_scatter_plot_2
 from utils import extract_tar_gz
-from ped_entry import PedEntry
 import os
 import mdtraj
 from featurizer import FeaturizationFactory
@@ -10,8 +9,7 @@ import numpy as np
 from dimensionality_reduction import DimensionalityReductionFactory
 
 DIM_REDUCTION_DIR = "dim_reduction"
-PDB_DIR = "pdb_data"
-TRAJ_DIR = "traj"
+TRAJ_DIR = "trajectories"
 
 class EnsembleAnalysis:
     def __init__(self, ens_codes, data_dir: str):
@@ -35,14 +33,14 @@ class EnsembleAnalysis:
         for ens_code in self.ens_codes:
             match = re.match(pattern, ens_code)
             if match:
+                print(f"Downloading entry {ens_code} from PED.")
                 ped_id = match.group(1)
                 ensemble_id = match.group(2)
                 tar_gz_filename = f'{ens_code}.tar.gz'
                 tar_gz_file = os.path.join(self.data_dir, tar_gz_filename)
 
-                pdb_dir = os.path.join(self.data_dir, PDB_DIR)
                 pdb_filename = f'{ens_code}.pdb'
-                pdb_file = os.path.join(pdb_dir, pdb_filename)
+                pdb_file = os.path.join(self.data_dir, pdb_filename)
 
                 if not os.path.exists(tar_gz_file) and not os.path.exists(pdb_file):
                     url = f'https://deposition.proteinensemble.org/api/v1/entries/{ped_id}/ensembles/{ensemble_id}/ensemble-pdb'
@@ -59,7 +57,7 @@ class EnsembleAnalysis:
 
                 # Extract the .tar.gz file
                 if not os.path.exists(pdb_file):
-                    extract_tar_gz(tar_gz_file, pdb_dir, pdb_filename)
+                    extract_tar_gz(tar_gz_file, self.data_dir, pdb_filename)
                     print(f"Extracted file {pdb_filename}.")
                 else:
                     print("File already exists. Skipping extracting.")
@@ -68,29 +66,42 @@ class EnsembleAnalysis:
 
 
     def generate_trajectories(self):
-        pdb_dir = os.path.join(self.data_dir, PDB_DIR)
         traj_dir = os.path.join(self.data_dir, TRAJ_DIR)
         os.makedirs(traj_dir, exist_ok=True)
-    
-        for ens_code in self.ens_codes:    
+        
+        for ens_code in self.ens_codes:
             pdb_filename = f'{ens_code}.pdb'
-            pdb_file = os.path.join(pdb_dir, pdb_filename)
-
+            pdb_file = os.path.join(self.data_dir, pdb_filename)
             traj_dcd = os.path.join(traj_dir, f'{ens_code}.dcd')
             traj_top = os.path.join(traj_dir, f'{ens_code}.top.pdb')
+            ens_dir = os.path.join(self.data_dir, ens_code)
 
-            # Generate trajectory from pdb if it doesn't exist, otherwise load it.
-            if not os.path.exists(traj_dcd) and not os.path.exists(traj_top):
-                print(f'Generating trajectory from {pdb_filename}.')
+            if os.path.exists(traj_dcd) and os.path.exists(traj_top):
+                print(f'Trajectory already exists for ensemble {ens_code}. Loading trajectory.')
+                trajectory = mdtraj.load(traj_dcd, top=traj_top)
+                self.trajectories[ens_code] = trajectory
+            elif os.path.exists(pdb_file):
+                print(f'Generating trajectory from PDB file: {pdb_file}.')
                 trajectory = mdtraj.load(pdb_file)
                 print(f'Saving trajectory.')
                 trajectory.save(traj_dcd)
                 trajectory[0].save(traj_top)
+                self.trajectories[ens_code] = trajectory
+            elif os.path.exists(ens_dir):
+                files_in_dir = [f for f in os.listdir(ens_dir) if f.endswith('.pdb')]
+                if files_in_dir:
+                    full_paths = [os.path.join(ens_dir, file) for file in files_in_dir]
+                    print(f'Generating trajectory from directory: {ens_dir}.')
+                    trajectory = mdtraj.load(full_paths)
+                    print(f'Saving trajectory.')
+                    trajectory.save(traj_dcd)
+                    trajectory[0].save(traj_top)
+                    self.trajectories[ens_code] = trajectory
+                else:
+                    print(f"No DCD files found in directory: {ens_dir}")
             else:
-                print(f'Trajectory already exists. Loading trajectory.')
-                trajectory = mdtraj.load(traj_dcd, top=traj_top)
-
-            self.trajectories[ens_code] = trajectory
+                print(f"File or directory for ensemble {ens_code} doesn't exist.")
+                return
 
     def perform_feature_extraction(self, featurization: str, normalize = False, *args, **kwargs):
         self.extract_features(featurization, *args, **kwargs)
--- refactored/ped_entry.py
+++
@@ -1,4 +0,0 @@
-class PedEntry:
-    def __init__(self, ped_id, ensemble_ids):
-        self.ped_id = ped_id
-        self.ensemble_ids = ensemble_ids
