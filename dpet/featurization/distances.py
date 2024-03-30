040098bbb5d4496b16f8097b3af4826c199a2a68
giacomo-janson <giacomo.janson@gmail.com>
2024-03-30 09:56:34 -0400

    Added center-of-mass (COM) distances.

--- dpet/featurization/distances.py
+++ dpet/featurization/distances.py
@@ -1,10 +1,11 @@
 import numpy as np
 import mdtraj
+from dpet.data.topology import slice_traj_to_com
 
 
 ca_selector = "protein and name CA"
 
-def calc_ca_dmap(traj: mdtraj.Trajectory):
+def _calc_dmap(traj: mdtraj.Trajectory):
     ca_ids = traj.topology.select(ca_selector)
     ca_xyz = traj.xyz[:, ca_ids]
     dmap = np.sqrt(
@@ -12,8 +13,15 @@ def calc_ca_dmap(traj: mdtraj.Trajectory):
     )
     return dmap
 
+def calc_ca_dmap(traj: mdtraj.Trajectory):
+    return _calc_dmap(traj=traj)
 
-def featurize_ca_dist(
+def calc_com_dmap(traj: mdtraj.Trajectory):
+    traj = slice_traj_to_com(traj)
+    return _calc_dmap(traj=traj)
+
+
+def _featurize_dist(
         traj: mdtraj.Trajectory,
         seq_sep: int = 2,
         inverse: bool = False,
@@ -40,3 +48,24 @@ def featurize_ca_dist(
         return ca_dist, names
     else:
         return ca_dist
+
+def featurize_ca_dist(
+        traj: mdtraj.Trajectory,
+        seq_sep: int = 2,
+        inverse: bool = False,
+        get_names: bool = True):
+    return _featurize_dist(traj=traj,
+                           seq_sep=seq_sep,
+                           inverse=inverse,
+                           get_names=get_names)
+
+def featurize_com_dist(
+        traj: mdtraj.Trajectory,
+        seq_sep: int = 2,
+        inverse: bool = False,
+        get_names: bool = True):
+    traj = slice_traj_to_com(traj)
+    return _featurize_dist(traj=traj,
+                           seq_sep=seq_sep,
+                           inverse=inverse,
+                           get_names=get_names)
