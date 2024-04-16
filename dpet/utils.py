51e04abb06cce929bd8101c55a494eec3ff33a21
Nikola Ivanović <nikola.iv.99@gmail.com>
2024-04-16 14:29:10 +0200

    Less error-prone extraction with renaming

--- dpet/utils.py
+++ dpet/utils.py
@@ -1,19 +1,11 @@
 import os
 import tarfile
 
-def extract_tar_gz(tar_gz_file, output_dir, new_name=None):
-    # Ensure the output directory exists
-    os.makedirs(output_dir, exist_ok=True)
-    
-    # Extract the .pdb file
+def extract_tar_gz(tar_gz_file:str, output_dir:str, new_name:str):
+    # Extract the .pdb file with renaming
     with tarfile.open(tar_gz_file, 'r:gz') as tar:
-        pdb_member = next((member for member in tar.getmembers() if os.path.splitext(member.name)[1] == '.pdb'), None)
-        if pdb_member:
-            # Set the extracted file name
-            extracted_name = new_name if new_name else 'pdbfile.pdb'
-            extracted_path = os.path.join(output_dir, extracted_name)
-            
-            # Extract the .pdb file
-            tar.extract(pdb_member, path=output_dir)
-            # Rename the extracted file
-            os.rename(os.path.join(output_dir, pdb_member.name), extracted_path)
+        for member in tar.getmembers():
+            if os.path.splitext(member.name)[1] == '.pdb':
+                member.name = new_name
+                tar.extract(member, path=output_dir)
+                break  # Only rename and extract the first .pdb file
